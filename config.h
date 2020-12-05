18265e36f2215798e9815db4d2ee3cfd2f176b99
yzia2000 <e0381439@u.nus.edu>
2020-12-05 14:03:30 +0530

    added volume control

--- config.h
+++ config.h
@@ -1,3 +1,4 @@
+#include <X11/XF86keysym.h>
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
@@ -62,10 +63,20 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *browsercmd[]  = { "brave", NULL };
+static const char *shutdowncmd[] = { "shutdown", "-h", "now", NULL };
+static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
+static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
+static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ 0,							XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
+	{ 0,							XF86XK_AudioMute, spawn, {.v = mutevol } },
+	{ 0,							XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,				XK_s,	   spawn,          {.v = shutdowncmd } },
+	{ MODKEY|ShiftMask,				XK_b,      spawn,          {.v = browsercmd } }, 
 	{ MODKEY,						XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
