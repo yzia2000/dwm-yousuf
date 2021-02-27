586916d0879122ecfc0316f1ad7ba5096f6f4a95
yzia2000 <e0381439@u.nus.edu>
2021-02-27 13:25:37 +0530

    changing border colors and adding hibernate script

--- config.def.h
+++ config.def.h
@@ -2,8 +2,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const int gappx     = 5;                 /* gaps between windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -11,8 +11,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "JetBrains Mono:antialias=true:autohint=true:size=10" };
+static const char dmenufont[]       = "JetBrains Mono:antialias=true:autohint=true:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -78,13 +78,14 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod4Mask
+#define MODALTKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} }, \
-	{ MODKEY|Mod4Mask,              KEY,      tagnextmon,     {.ui = 1 << TAG} }, \
-	{ MODKEY|Mod4Mask|ShiftMask,    KEY,      tagprevmon,     {.ui = 1 << TAG} },
+	{ MODKEY|MODALTKEY,              KEY,      tagnextmon,     {.ui = 1 << TAG} }, \
+	{ MODKEY|MODALTKEY|ShiftMask,    KEY,      tagprevmon,     {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
@@ -94,22 +95,15 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *rangercmd[]  = { "st", "-e", "ranger", NULL };
-static const char *browsercmd[]  = { "brave", NULL };
+static const char *browsercmd[]  = { "firefox-developer-edition", NULL };
 static const char *shutdowncmd[] = { "shutdown", "-h", "now", NULL };
 static const char *upvol[]   = { "/home/yushi/.config/dwm/scripts/volu", NULL };
 static const char *downvol[] = { "/home/yushi/.config/dwm/scripts/vold", NULL };
+static const char *mutemiccmd[] = { "/home/yushi/.config/dwm/scripts/togglemic", NULL };
 static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ 0,							XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
-	{ 0,							XF86XK_AudioMute, spawn, {.v = mutevol } },
-	{ 0,							XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
-	{ MODKEY|ShiftMask,				XK_f,	   spawn,		   {.v = rangercmd } },
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,				XK_s,	   spawn,          {.v = shutdowncmd } },
-	{ MODKEY|ShiftMask,				XK_b,      spawn,          {.v = browsercmd } }, 
-	{ MODKEY,						XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -146,6 +140,16 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+    // Custom Shortcuts
+	{ 0,							XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
+	{ 0,							XF86XK_AudioMute, spawn, {.v = mutevol } },
+	{ 0,							XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
+	{ MODKEY|ShiftMask,				XK_f,	   spawn,		   {.v = rangercmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,				XK_s,	   spawn,          {.v = shutdowncmd } },
+	{ MODKEY|ShiftMask,				XK_b,      spawn,          {.v = browsercmd } }, 
+	{ MODKEY,						XK_Return, spawn,          {.v = termcmd } },
+    { MODKEY|ControlMask,           XK_m,      spawn,          {.v = mutemiccmd } }
 };
 
 /* button definitions */
